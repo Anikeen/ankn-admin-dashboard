eb464265dad33fdca79518832fb31b2d679360b0
Petr Anikin <p.anikeen@gmail.com>
2022-03-10 14:32:24 +0300

    feat: firebase authentication added.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import { AuthContext } from "./context";
 import { GlobalStyles } from "./styles/global";
 import { Login } from "./views/auth/Login";
 
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 
 export const App = () => {
   const [isAuth, setIsAuth] = useState(false);
 
+  const auth = getAuth();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) setIsAuth(true);
+  });
+
   const signIn = (login: string, password: string) => {
-    setIsAuth(true);
+    signInWithEmailAndPassword(auth, login, password)
+      .then((userCredential) => {
+        setIsAuth(true);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
   }
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from './App';
 
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+import { firebaseConfig } from './.env.js';
+
+initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <Router>
     <App />
