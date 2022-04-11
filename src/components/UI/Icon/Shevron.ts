cd0f22cf9a8b13f485e1d9f7ac46a83ceba6a4ad
Petr Anikin <p.anikeen@gmail.com>
2022-04-11 12:56:08 +0300

    refactor: some styles changed.

--- src/components/UI/Icon/Shevron.ts
+++ src/components/UI/Icon/Shevron.ts
@@ -5,6 +5,6 @@ export const Shevron = styled.span<{ isShown: boolean }>(({ isShown }) =>
   display: block;
   margin-left: auto;
   font-size: 1.3rem;
-  transform: ${isShown ? 'rotate(0deg)' : 'rotate(180deg)'};
+  transform: ${isShown ? 'rotate(180deg)' : 'rotate(0deg)'};
   transition: transform .4s;
 `)
